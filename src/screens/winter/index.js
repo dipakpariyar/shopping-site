6b3c980400bb2dbcd0fa62411440730ad23f216d
dipakpariyar <dipakpriyar499@gmail.com>
2021-07-01 19:36:30 +0545

    adding component and resizing

---
+++ src/screens/winter/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Carousel from '../../common/Carousel/Horizontalcarousel';
+
+export default function Winter() {
+  const data=[
+    '/assets/winter/electric kettle.jpeg',
+    '/assets/winter/electric heater.jpeg',
+    '/assets/winter/electric jug.jpeg',
+    '/assets/winter/good heater.jpg',
+    '/assets/winter/winter and sumer heater.jpg',
+    '/assets/winter/ovan.jpeg',
+  ]
+  return (
+    <div className="winter_brand">
+      <h2 className="winter_text">Winter</h2>
+
+
+      <div className="winter_carousel_div">
+        <div className="winter_carousel">
+          <Carousel data={data} />
+        </div>
+      </div >
+    </div>
+  )
+}
