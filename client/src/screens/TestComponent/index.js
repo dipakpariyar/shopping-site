624a14544048cfbff04631ec4789e0ddbf0c122f
bhayasah <bhagyasah4u@gmail.com>
2021-08-01 11:23:55 +0545

    magnifier problem solved

--- client/src/screens/TestComponent/index.js
+++ client/src/screens/TestComponent/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
-import { SideBySideMagnifier } from 'react-image-magnifiers';
+import ReactImageMagnify from 'react-image-magnify';
+
+const url = '/assets/graphics/images/fan.jpg';
 
 export default class Test extends React.Component {
 
   render() {
     return (
-      <SideBySideMagnifier
-      imageSrc="/assets/graphics/images/fan.jpg"
-      imageAlt="Example"
-      // largeImageSrc="./large-image.jpg"
-    />
+      <div style={{ height: 400, width: 400 }}>
+      <ReactImageMagnify {...{
+        smallImage: {
+            alt: 'Wristwatch by Ted Baker London',
+            isFluidWidth: true,
+            src: url
+        },
+        largeImage: {
+            src: url,
+            width: 300,
+            height: 300
+        }
+    }} />
+    </div>
+    // "test"
     )
   }
 }
